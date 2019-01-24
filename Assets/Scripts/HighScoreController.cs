fece70f99aac08a73ba01bbd36472e68179bcbad
smeerws <hello@smeerws.at>
2019-01-24 01:01:23 +0100

    1 simple example scene 101 Scene save/load data

---
+++ Assets/Scripts/HighScoreController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScoreController : MonoBehaviour {
+
+    public static HighScoreController instance;
+
+    public Text text;
+
+    float highScore = 0; 
+
+    //Singelton Pattern
+    void Awake()
+    {
+        if (instance == null)
+        {
+            Debug.Log("CREATE INSTANCE");
+            instance = this;
+        }
+        else if (instance != this) {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private float GetRandomHighscore()
+    {
+        Debug.Log("Button Do Something");
+        return Random.Range(0f, 1000f);
+    }
+
+    public float GetHighScore()
+    {
+        return highScore;
+    }
+
+    public void SetHighScore(float val)
+    {
+        highScore = val;
+    }
+
+    public void ResetHighScore()
+    {
+        highScore = 0;
+        text.text = highScore.ToString();
+    }
+
+    public void SetDisplayRandomScore()
+    {
+        Debug.Log("Generate Random Highscore");
+        highScore = GetRandomHighscore();
+
+        Debug.Log("Set Text");
+        text.text = highScore.ToString();
+
+        SaveGameData saveGameData = new SaveGameData();
+        saveGameData.Save();
+    }
+
+    public void LoadHighScore()
+    {
+        SaveGameData.Load();
+        text.text = highScore.ToString();
+    }
+
+}
---
+++ Assets/Scripts/SaveGameData.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using GameDevProfi.Utils;
+using System.IO;
+
+[System.Serializable] //Mark that this class is able to read/write data
+public class SaveGameData
+{
+    //all member (instance) variables will be saved;
+    public float highscore = -1f; //declare and inizialize (with default value) variable highscore
+    //public int test;
+
+    private static string GetFilename()
+    {
+        //Path.DirectorySeparatorCha --> / Linux/Mac, \ Windwos
+        return Application.persistentDataPath + Path.DirectorySeparatorChar + "savegame.xml";
+    }
+
+    //Speichert Spielstand
+    public void Save()
+    {
+        Debug.Log("Save Data: " + GetFilename());
+
+        HighScoreController highscoreController = Component.FindObjectOfType<HighScoreController>(); //returns first GameObject HighScoreController in scene 
+        highscore = highscoreController.GetHighScore(); // gets the current highscore saved in the HighscoreController GameObject
+
+        // test = 123;
+        string xml = XML.Save(this); //takes the XML Class --> using GameDevProfi.Utils
+        File.WriteAllText(GetFilename(), xml);
+        Debug.Log(xml);
+    }
+
+    //Lädt Spielstand
+    public static SaveGameData Load()
+    {
+        Debug.Log("Load Data: " + GetFilename());
+
+        SaveGameData save = XML.Load<SaveGameData>(File.ReadAllText(GetFilename()));
+        HighScoreController highscoreController = Component.FindObjectOfType<HighScoreController>();
+        highscoreController.SetHighScore(save.highscore);
+
+        return save;
+    }
+
+}
